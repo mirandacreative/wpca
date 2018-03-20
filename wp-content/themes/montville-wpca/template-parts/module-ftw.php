f93ab17fcbbada1f65b40279a23f659574d80529
Brian Pearson <traffic@Carloss-MBP.localdomain>
2018-03-20 12:05:17 -0400

    added ftw a fullwidthj module

---
+++ wp-content/themes/montville-wpca/template-parts/module-ftw.php
@@ -0,0 +1,12 @@
+<div class="module module-<?php echo $acf_fc_layout;?>">
+	<div class="container">
+		<div class="content text-center">
+			<?php if($title) : ?>
+				<h2 class="ignore-br"><?= $title; ?></h2>
+			<?php endif; ?>
+			<?php if($blurb) : ?>
+				<div class="blurb ignore-br"><?= $blurb; ?></div>
+			<?php endif; ?>
+		</div>
+	</div>
+</div>
